7de15bce2b19829e73760a871aaac412980cdef1
Stiven Peter <Peter@Stivens-MBP.fios-router.home>
2017-02-13 22:58:21 -0500

    Work

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
-var c = document.getElementByID("slate")
-var ctx=c.getContext("2d")
-
-var makeRectangle = function(x, y, w, h){
+var c = document.getElementById("slate");
+var ctx = c.getContext("2d");
+var makeRectangle = function(e) {
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
     ctx.fillStyle = "#ff0000";
-    ctx.fillRect(x,y,w,h);
+    ctx.fillRect(mouseX,mouseY,100,100);
 };
-
-var getCursorPosition = function(canvas, event) {
-    var rect = canvas.getBoundingClientRect();
-    var x = event.clientX - rect.left;
-    var y = event.clientY - rect.top;
-    console.log("x: " + x + " y: " + y);
-    makeRectangle(x,y,100,100);
-};
-
-var clearCanvas = function(){
-    ctx.clearRect(0,0,c.width, c.height);
-};
-
-c.addEventListener('click', getCursorPosition(c, 'click'))
-
-document.getElementByID("clear").addEventListener('click', clearCanvas)
+var clearCanvas = function() {
+    ctx.clearRect(0,0,c.width,c.height);
+ };
+c.addEventListener("click", makeRectangle);
+document.getElementById("clear").addEventListener("click", clearCanvas);
